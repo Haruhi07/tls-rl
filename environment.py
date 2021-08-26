926dc8add33ad8cdd6e89f5c45272d8003e0031f
Haruhi07 <yuxuan.ye@outlook.com>
2021-08-26 02:56:15 +0100

    debug rl

--- environment.py
+++ environment.py
@@ -7,12 +7,12 @@ class Environment:
 
     def count_keyword(self, text):
         word_list = text.lower().split()
-        #ret = 0
-        #for word in word_list:
-        #    if word in self.keywords:
-        #        ret += 1
-        #return ret
-        return len(set(word_list) & self.keywords)
+        ret = 0
+        for word in word_list:
+            if word in self.keywords:
+                ret += 1
+        return ret
+        #return len(set(word_list) & self.keywords)
     
     def calc_reward(self):
         text = env_utils.concatenate(self.timelines)

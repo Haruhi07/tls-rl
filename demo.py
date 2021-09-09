3164851e63aee41d86f04a50b46d5f3d5341c2fc
Haruhi07 <yuxuan.ye@outlook.com>
2021-09-09 21:00:48 +0100

    add demo

--- demo.py
+++ demo.py
@@ -8,16 +8,23 @@ dataset_version = '3.0.0'
 model_name = 'sshleifer/distill-pegasus-cnn-16-4'
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-dataset = load_dataset(dataset_name, dataset_version, split='train')
+dataset = load_dataset(dataset_name, dataset_version, split='test')
 print(dataset)
 tokenizer = PegasusTokenizer.from_pretrained(model_name)
 model = PegasusForConditionalGeneration.from_pretrained(model_name).to(device)
 
-dataset = dataset.map(lambda e: tokenizer(e['article'], truncation=True, padding='max_length'), batched=True)
 dataset = dataset.map(lambda e: tokenizer(e['highlights'], truncation=True, padding='max_length'), batched=True)
-dataset.set_format(type='torch', columns=['article', 'highlights', 'id'])
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=32)
+dataset = dataset.map(lambda e: {'labels': e['input_ids']})
+dataset = dataset.map(lambda e: tokenizer(e['article'], truncation=True, padding='max_length'), batched=True)
+dataset.set_format(type='torch', columns=['input_ids', 'attention_mask', 'labels'])
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=4)
+
+max_loss = 0
 
 for it in dataloader:
-    print(it)
-    break
+    result_dict = model(**it)
+    loss_list = result_dict.loss
+    batch_maxloss = max(loss_list)
+    max_loss = max(max_loss, batch_maxloss)
+
+print("max_loss = ", max_loss)

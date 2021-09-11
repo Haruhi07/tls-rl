3f807fd4365f7914aba8a96a01bf6f4381a7b351
Haruhi07 <yuxuan.ye@outlook.com>
2021-09-12 00:37:47 +0100

    add demo

--- demo.py
+++ demo.py
@@ -8,7 +8,8 @@ dataset_name = 'cnn_dailymail'
 dataset_version = '3.0.0'
 model_name = 'sshleifer/distill-pegasus-cnn-16-4'
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-cache_dir = pathlib.Path('/work/hs20307/huggingface')
+#cache_dir = pathlib.Path('/work/hs20307/huggingface')
+cache_dir = pathlib.Path('~/.cache/huggingface')
 
 dataset = load_dataset(dataset_name, dataset_version, split='test', cache_dir=cache_dir/'datasets')
 print(dataset)
@@ -16,15 +17,18 @@ tokenizer = PegasusTokenizer.from_pretrained(model_name, cache_dir=cache_dir/'tr
 model = PegasusForConditionalGeneration.from_pretrained(model_name, cache_dir=cache_dir/'transformers').to(device)
 
 dataset = dataset.map(lambda e: tokenizer(e['highlights'], truncation=True, padding='max_length'), batched=True)
-dataset = dataset.map(lambda e: {'labels': e['input_ids'].to(device)})
-dataset = dataset.map(lambda e: tokenizer(e['article'], truncation=True, padding='max_length').to(device), batched=True)
+dataset = dataset.map(lambda e: {'labels': e['input_ids']})
+dataset = dataset.map(lambda e: tokenizer(e['article'], truncation=True, padding='max_length'), batched=True)
 dataset.set_format(type='torch', columns=['input_ids', 'attention_mask', 'labels'])
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=4)
 
 max_loss = 0
 
 for it in dataloader:
-    result_dict = model(**it)
+    input_ids = it['input_ids'].to(device)
+    attention_mask = it['attention_mask'].to(device)
+    labels = it['labels'].to(device)
+    result_dict = model(input_ids=input_ids, attention_mask=attention_mask, labels=labels)
     loss_list = result_dict.loss
     batch_maxloss = max(loss_list)
     max_loss = max(max_loss, batch_maxloss)
